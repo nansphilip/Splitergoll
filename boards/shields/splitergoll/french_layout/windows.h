390e27f8f19cf6b2972ecda1588d229de71744c4
Nans PHILIP <nansphilip@gmail.com>
2024-03-10 23:03:39 +0100

    Push

--- boards/shields/splitergoll/french_layout/windows.h
+++ boards/shields/splitergoll/french_layout/windows.h
@@ -24,7 +24,7 @@ MACRO(name, \
     ; \
 )
 
-#define MACRO_ALT_CODE(name, d1, d2, d3, d4) \
+#define MACRO_ALT_CODE_PLUS(name, d1, d2, d3, d4) \
 MACRO(name, \
   bindings \
     = <&macro_press &kp LALT> \
@@ -44,10 +44,10 @@ MACRO_ALT_CODE(win_etre,    1, 3, 7) // ë
 MACRO_ALT_CODE(win_ocir_up, 2, 1, 2) // Ô
 MACRO_ALT_CODE(win_ocir,    1, 4, 7) // ô
 MACRO_ALT_CODE(win_icir_up, 1, 4, 0) // î
-MACRO_ALT_CODE(win_icir, 0, 2, 0, 6) // Î
+MACRO_ALT_CODE_PLUS(win_icir,    0, 2, 0, 6) // Î
 MACRO_ALT_CODE(win_itre_up, 1, 3, 9) // ï
-MACRO_ALT_CODE(win_itre, 0, 2, 0, 7) // Ï
-MACRO_ALT_CODE(win_ugrv_up, 0, 2, 1, 7) // Ù
+MACRO_ALT_CODE_PLUS(win_itre,    0, 2, 0, 7) // Ï
+MACRO_ALT_CODE_PLUS(win_ugrv_up, 0, 2, 1, 7) // Ù
 MACRO_ALT_CODE(win_oe_lo,   1, 5, 6) // œ
 MACRO_ALT_CODE(win_oe_up,   1, 4, 0) // Œ
 MACRO_ALT_CODE(win_grave,   0, 9, 6) // `
