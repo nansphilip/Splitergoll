ec459b35b9171a34d13ac052d52d2875c5375afc
Nans PHILIP <nansphilip@gmail.com>
2024-03-10 20:32:21 +0100

    Push

--- boards/shields/splitergoll/french_layout/function.h
+++ boards/shields/splitergoll/french_layout/function.h
@@ -6,3 +6,14 @@
         ZMK_MACRO(__VA_ARGS__) \
     }; \
 };
+
+#define LAYER(name, layout, sensor) \
+/ { \
+    keymap { \
+        compatible = "zmk,keymap"; \
+        name##_layer { \
+            bindings = <layout>; \
+            sensor-bindings = <sensor>; \
+        }; \
+    }; \
+};
