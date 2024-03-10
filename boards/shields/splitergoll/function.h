9d6dd870268ba373b963e72ea34218c2d010f4ed
Nans PHILIP <nansphilip@gmail.com>
2024-03-10 20:09:47 +0100

    Push

--- boards/shields/splitergoll/function.h
+++ boards/shields/splitergoll/function.h
@@ -1,11 +1,2 @@
 #pragma once
 
-#define LAYER(name, layout, sensors) / { \
-    keymap { \
-        compatible = "zmk,keymap"; \
-        name##_layer { \
-            bindings = <layout>; \
-            sensors = <sensors>; \
-        }; \
-    }; \
-};
