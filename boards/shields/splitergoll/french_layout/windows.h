19853b8a0eafab5d0d8c711ad5bb610920b6d3bc
Nans PHILIP <nansphilip@gmail.com>
2024-03-11 02:05:51 +0100

    Push

--- boards/shields/splitergoll/french_layout/windows.h
+++ boards/shields/splitergoll/french_layout/windows.h
@@ -5,7 +5,7 @@
 #define MACRO_DOUBLE(name, d1, d2) MACRO(name, bindings \
 = <&macro_tap &kp KP_N##d1 &kp KP_N##d2>;)
 
-#define MACRO_DOUBLE_SHIFT(name, d1, d2) MACRO(name, bindings \
+#define MACRO_DOUBLE_SHIT(name, d1, d2) MACRO(name, bindings \
 = <&macro_tap &kp KP_N##d1>, \
 <&macro_press &kp LSFT>, \
 <&macro_tap &kp KP_N##d2>, \
