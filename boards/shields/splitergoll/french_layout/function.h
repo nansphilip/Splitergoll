ab3e20ef6228b660873f910d0d14d90e9942464e
Nans PHILIP <nansphilip@gmail.com>
2024-03-11 14:31:41 +0100

    Push

--- boards/shields/splitergoll/french_layout/function.h
+++ boards/shields/splitergoll/french_layout/function.h
@@ -1,16 +1,18 @@
 #pragma once
 
+
+
 #define MORPH(name, k1, mod, k2) \
-behaviors { \
-    name: name { \
-        compatible = "zmk,behavior-mod-morph"; \
-        #binding-cells = <0>; \
-        bindings = <k1>, <k2>; \
-        mods = <(MOD_##mod)>; \
+/ { \
+    behaviors { \
+        name: name { \
+            compatible = "zmk,behavior-mod-morph"; \
+            #binding-cells = <0>; \
+            bindings = <k1>, <k2>; \
+            mods = <(MOD_##mod)>; \
+        }; \
     }; \
 };
-
-#define MORPH_UP(name, k1, k2) MORPH(name, k1, LSFT, k2)
 
 #define MACRO(...) \
 / { \
