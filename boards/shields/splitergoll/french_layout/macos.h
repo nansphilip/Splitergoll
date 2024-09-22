a784e03508f30726598bc63e0fc1868322b5ca80
nansphilip <nansphilip@gmail.com>
2024-09-22 10:55:26 +0200

    Clean

---
+++ boards/shields/splitergoll/french_layout/macos.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "../function.h"
+#include "windows.h"
+
+// Modifiers
+#define M_CTL  LCTRL
+#define M_OPN  LALT
+#define M_CMD  LGUI
+
+
+// Punctuation
+#define M_EXCL  N8              // !
+#define M_BSLASH  LS(RA(DOT))   // backslash
+
+
+// Calculus
+// #define M_PLUS  LS(SLASH)     // +
+// #define M_MINUS EQUAL         // -
+// #define M_ASTR  LS(RBKT)      // *
+// #define M_EQUAL SLASH         // =
+
+// #define M_AT    GRAVE         // @
+// #define M_HASH  LS(GRAVE)     // #
+// #define M_PIPE  RA(L)         // |
+// #define M_UNDS  LS(EQUAL)     // _
+
+// #define M_APOS  LS(RA(N4))    // ’
+// #define M_MICR  RA(SEMI)      // µ
+
+// #define M_EURO  RA(RBKT)      // €
+
+
+// #define M_LCBR  RA(N5)        // {
+// #define M_RCBR  RA(MINUS)     // }
+// #define M_LBRC  LS(RA(N5))    // [
+// #define M_RBRC  LS(RA(MINUS)) // ]
+
+// #define M_GRAVD NUHS          // ` (dead)
+// #define M_ACUTE LS(RA(N1))    // ´ (dead)
